25ad3802bcce5b3e24fa6aa0b838868717f7accd
Oussama Boujnan <69466099+Oussama-pixel@users.noreply.github.com>
2021-06-09 15:52:45 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const [query,setQuery] = useState('chicken');
   useEffect(()=>{
     getRecipes();
-  }, [query]);
+  });
   const getRecipes= async()=>{
     const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`);
     const data = await response.json();
